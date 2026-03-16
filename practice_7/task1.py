dc9154afde60b863da82beb4a2a240fe2db6de39
vildan222 <vildanarmakov6@gmail.com>
2026-03-16 08:49:37 +0500

    Create task1.py

---
+++ practice_7/task1.py
@@ -0,0 +1,7 @@
+count = int(input("Число участников :\n>>>"))
+for i in range(count):
+  name = input("Введите имя:\n>>>")
+  print(f"Добро пожаловать, {name}")
+print("Групповой чат создан!")  
+
+               
